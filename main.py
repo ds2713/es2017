9ec475ecd2dd7e6847f31e40f29698168cfc59ea
Meng Kiang Seah <mengkiang92@gmail.com>
2017-02-02 21:47:23 +0000

    Changes to main.py. Removed unnecessary code. Tidied up preamble output. Added line to reinitialise historic_reg since after processing, it contains only old info.

--- main.py
+++ main.py
@@ -11,13 +11,20 @@ def main():
 	sta_if = network.WLAN(network.STA_IF)
 	sta_if.active(True)
 	sta_if.connect('EEERover', 'exhibition')
-	time.sleep(5)
+
+	# While waiting for a connection, print and wait.
+	while sta_if.ifconfig()[0] == '0.0.0.0':
+		print("Connecting...")
+		time.sleep(0.5)
+
+	# Print successful network IP address.
+	print("Network connected. IP Address: " + sta_if.ifconfig()[0])
 
 	# Confirming network connection successful
-	print(sta_if.ifconfig())
+	# print(sta_if.ifconfig())
 
-	# FTP Server
-	import uftpd
+	# FTP Server, left out because ampy!
+	# import uftpd
 
 	# Initial MQTTClient
 	CLIENT_ID = 'mdma'
@@ -25,7 +32,8 @@ def main():
 	client = MQTTClient(CLIENT_ID, '192.168.0.10')
 	client.connect()
 	# Send MQTT Message at boot to confirm success
-	client.publish(CLIENT_TOPIC, bytes('MDMA live', 'utf-8'))
+	client.publish(CLIENT_TOPIC, bytes('MDMA MQTT Live', 'utf-8'))
+	print("MQTT client successful.")
 
 	# Setup i2c class for interface with ADC
 	i2c = I2C(scl=Pin(5), sda=Pin(4), freq = 100000)
@@ -33,6 +41,7 @@ def main():
 	numBytes = 1
 	# ADC configuration
 	ads = ads1x15.ADS1115(i2c, address=72)
+	print("ADC configured.")
 
 	# Initialise parameters for measurements
 	samples = 100
@@ -40,23 +49,18 @@ def main():
 	future = samples - history - 1
 	threshold = 50
 
-	# Initialise registers and pointers
+	# Initialise registers and pointer
 	output_reg = [0]*samples
 	historic_reg = [0]*history
-	reading_pointer = 0
 	hist_pointer = 0
 
+	print("Begin reading values.")
 	# Infinite reading loop
 	while True:
 
 		reading = ads.read(0)
 
 		if math.fabs(reading) > threshold:
-
-			# Copy history into output register
-			# for x in range (0, history):
-			# 	output_reg[x] = historic_reg[(hist_pointer+1)%history]
-			# 	hist_pointer = (hist_pointer+1)%history
 
 			# Add current reading to output register
 			output_reg[history+1] = reading;
@@ -70,7 +74,7 @@ def main():
 			output_reg[history - hist_pointer: history] = historic_reg[0:hist_pointer]
 
 			# For debugging, print output register to terminal
-			print('New measurement: ', output_reg)
+			print('New measurement:', output_reg)
 
 			# Acquire maximum reading in output register
 			maximum_value = max(output_reg)
@@ -78,17 +82,15 @@ def main():
 			# Send to broker
 			client.publish(CLIENT_TOPIC, bytes(output_str, 'utf-8'))
 
+			# Reinitialise historic_reg and pointer
+			# Because it contains outdated info.
+			historic_reg = [0]*history
+			hist_pointer = 0
+
 		else:
 			# Implementation of circular buffer for historical readings
 			historic_reg[hist_pointer] = reading
 			hist_pointer = (hist_pointer+1)%history
 
 if __name__ == "__main__":
-	# execute only if run as a script
-
-
-
 	main()
-
-# except:
-# 	print('Stopped')
